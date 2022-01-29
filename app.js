d4671370e2fa9435fdf1e7109a1246620a4441d7
Alberta McAleavey <a.mcaleavey24@gmail.com>
2022-01-29 11:20:11 -0500

    complete task 3

--- app.js
+++ app.js
@@ -9,22 +9,26 @@ const game = {
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
   },
   getGuess: function() {
-    let currentGuess = null
-    while(currentGuess !== this.secretNum){
-      currentGuess = parseInt(prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum} :`))
+    let input = null
+    while(input !== this.secretNum){
+      input = prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum} :`)
+      let currentGuess = parseInt(input)
+      console.log(currentGuess)
+
       // return currentGuess
       
     }
 
-    // currentGuess points to the user input of the prompt message that appears in the window
-    // if the currentGuess is not the same as secretNum, the while loop continues to prompt the user to enter a guess until it is
+    // input points to the user input of the prompt message that appears in the window
+    // parseInt returns the input of the prompt as a number, currentGuess = the number the user entered
+    // if the input is not the same as secretNum, the while loop continues to prompt the user to enter a guess until it is
     
   }
   
 }
-
+      
+      
+console.log(game.play())
 console.log(game.getGuess())
 
 // getGuess should return the value of the current guess
-
-//why is currentGuess = parseInt(prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum} :`)) undefined?
